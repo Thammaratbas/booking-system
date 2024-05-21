bc331f0e241214816bfd5436d630f36616577e78
Thammaratbas <135933619+Thammaratbas@users.noreply.github.com>
2024-05-21 16:22:41 +0700

    hi
    -

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+class Config {
+    private $host = 'localhost';
+    private $dbname = 'login_db';
+    private $username = 'root';
+    private $password = '';
+    protected $conn = null;
+
+    public function __construct()
+    {
+        try {
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+            // Set the PDO error mode to exception
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+}
